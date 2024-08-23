b42d229e034930271288bd670baa17a0a847633b
Jonathan Guzmán <jonguz5@hotmail.com>
2024-08-22 22:06:48 -0600

    add parameters to main script

--- samples/main.ps1
+++ samples/main.ps1
@@ -3,9 +3,20 @@ using module ./Cat.psm1
 using module ./Dachshund.psm1
 using module ./Dog.psm1
 
+param(
+    [string]$DogName = "Rex",
+    [string]$CatName = "Whiskers"
+)
+
 $ErrorActionPreference = "Stop"
 
 [Animal]$ADog = [Daschund]::new()
 [Animal]$ACat = [Cat]::new()
+if ($DogName) {
+    $ADog.name = $DogName
+}
+if ($CatName) {
+    $ACat.name = $CatName
+}
 Write-Host "$($ADog.name) says: $($ADog.Speak())"
 Write-Host "$($ACat.name) says: $($ACat.Speak())"
